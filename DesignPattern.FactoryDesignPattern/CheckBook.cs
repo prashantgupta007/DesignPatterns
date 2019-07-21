fc0b683d9dd19bc5c50480d12212000277171954
Prashant Gupta <prashant.siet@gmail.com>
2019-07-21 18:33:22 +0530

    First Commit

---
+++ DesignPattern.FactoryDesignPattern/CheckBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.FactoryDesignPattern
+{
+    public class CheckBook
+    {
+        protected decimal _Amount;
+        public decimal getExpense()
+        {
+            return _Amount;
+        }
+    }
+}
